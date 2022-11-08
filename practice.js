2d1be8952acfe516f90b5249ed8966ea55bdb7ed
Nuhaa1234 <104890324+Nuhaa1234@users.noreply.github.com>
2022-11-08 19:08:28 +1100

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,20 @@
+var paddle2= 10, paddle1= 10;
+
+var paddle1X= 10, paddle1Height= 110;
+var paddle2Y= 865, paddle2Height= 70;
+
+var score1= 0, score2= 0;
+var paddle1Y= 0;
+
+var playerscore= 0;
+
+var pcscore= 0;
+
+var ball = 
+{
+    x:350/2,
+    y:480/2,
+    r:20,
+    dx:3,
+    dy:3
+}
